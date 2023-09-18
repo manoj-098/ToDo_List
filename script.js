b171f1d9257c260493d3ba844f2c42887dd8d822
Manoj V <manojvenki098@gmail.com>
2023-09-18 16:07:31 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,70 @@
+ const input=document.getElementById("input");
+        const ul=document.getElementById("ul");
+
+        //Array for storing the inputs
+        let todos=[];
+
+        document.getElementById("btn").addEventListener('click',
+        function()
+        {
+            //Adding input to the array
+            if(input.value!=''){
+            todos.push(input.value);
+            addTodo(input.value);
+            input.value='';
+            }
+            else
+            {
+                alert("Enter Something");
+            }
+        }
+        );
+        function addTodo(todo)
+        {
+            let li=document.createElement("li");
+            let para=document.createElement("p");
+            let btn1=document.createElement("button");
+            let btn2=document.createElement("button");
+            btn1.innerHTML="<img src='images/tick.svg' class='img'/>";
+            btn2.innerHTML="<img src='images/delete.svg' class='img'/>";
+
+            //Appending attributes
+            li.appendChild(btn1);
+            console.log(todos);
+            para.innerHTML=input.value;
+            li.appendChild(para);
+            li.appendChild(btn2);
+            ul.appendChild(li);
+
+
+            //Functionalities of button
+            btn1.addEventListener('click',
+            function()
+            {
+                para.style.textDecoration="line-through";
+                li.style.opacity=0.5;
+                Remove(todo);
+            }
+            );
+
+            btn2.addEventListener('click',
+            function()
+            {
+                li.remove();
+                Remove(todo);
+            }
+            );
+        }
+
+        function Remove(todo)
+        {
+            
+            let index=todos.indexOf(todo);
+            if(index>=0)
+            {
+            todos.splice(index,1);
+            console.log(todos);
+    
+            }
+
+        }

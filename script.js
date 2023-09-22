87540b2de8d2f5c3ac2c206dff4fb859966b5bea
Manoj V <manojvenki098@gmail.com>
2023-09-22 14:24:56 +0530

    Update script.js

--- script.js
+++ script.js
@@ -35,17 +35,36 @@
             li.appendChild(para);
             li.appendChild(btn2);
             ul.appendChild(li);
-
-
-            //Functionalities of button
+        
+             //Functionalities of button
+            let click=1;
             btn1.addEventListener('click',
             function()
             {
-                para.style.textDecoration="line-through";
-                li.style.opacity=0.5;
-                Remove(todo);
-            }
-            );
+        
+            if(click==0)
+        {
+              para.style.textDecoration="none";
+              li.style.opacity=1;
+              btn1.innerHTML="<img src=images/tick.svg class='img'/>";
+              click=1;
+        }
+        else{
+            let click=clicked();
+            para.style.textDecoration="line-through";
+            li.style.opacity=0.5;
+            btn1.innerHTML="<img src=images/unselect.svg class='img'/>";
+            Remove(todo);
+        }
+        
+    }
+    );
+    function clicked()
+    {
+          click=0;
+    }
+
+            
 
             btn2.addEventListener('click',
             function()
